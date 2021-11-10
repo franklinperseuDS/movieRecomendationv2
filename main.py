08f85dd0a2f541d336f36c51aad7659bf6ca8c07
Franklin Perseu de Lima e Lima <franklin.lima@grupois.mao>
2021-11-10 07:21:51 -0400

    gerando a rota com methodo post

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask
 import numpy as np
 from sklearn.model_selection import train_test_split
-from flask import Flask, request, jsonfy
+from flask import Flask, request, jsonify
 
 
 #criar app
@@ -10,15 +10,24 @@ app = Flask(__name__)
 
 
 #coluna de dados
-colunas = ['filmeId','nota_media','Horror','Crime','Children','Thriller','Film-Noir','Documentary','Fantasy','Mystery',
-           'Comedy','Drama','Musical','Adventure','Romance','War','Animation','IMAX','Action','Western','Sci-Fi','class',  ]
-
+#colunas = ['filmeId','nota_media','Horror','Crime','Children','Thriller','Film-Noir','Documentary','Fantasy','Mystery',
+#           'Comedy','Drama','Musical','Adventure','Romance','War','Animation','IMAX','Action','Western','Sci-Fi','class',  ]
+colunas = ['id','filmeId']
 
 
 @app.route('/reco/<id>')
 def show_id(id):
     return f'Recebendo dados \n ID: {id}'
 
+@app.route('/reco/', methods=['POST'])
+def get_score():
+    dados = request.get_json()
+    # playload = np.array([dados[col] for col in colunas])
+    
+    if(dados):
+        return f'chegou aqui {dados}'
+    else:
+        return 'nada aqui'
 
 @app.route('/')
 def home():
