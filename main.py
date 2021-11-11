0ca2b027f3eff39c803724573ded99917ff32aef
perseu-at-237228232680 <69854802+franklinperseuDS@users.noreply.github.com>
2021-11-10 22:32:28 -0400

    adicionou o modelo e o csv de filmes

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 from flask import Flask, request, jsonify
 import pickle
 
 #criar app
 app = Flask(__name__)
 
-
+#movies
+movies = pd.read_csv("movies.csv")
 #coluna de dados
-#colunas = ['filmeId','nota_media','Horror','Crime','Children','Thriller','Film-Noir','Documentary','Fantasy','Mystery',
-#           'Comedy','Drama','Musical','Adventure','Romance','War','Animation','IMAX','Action','Western','Sci-Fi','class',  ]
-colunas = ['id','filmeId']
+colunas = ['filmeId', 'nota_media', 'Drama', 'Documentary', 'Romance', 'Action',
+       'Adventure', 'IMAX', 'Horror', 'War', 'Musical', 'Sci-Fi', 'Fantasy',
+       'Animation', 'Film-Noir', 'Crime', 'Mystery', 'Children', 'Comedy',
+       'Western', 'Thriller']
+
+#colunas = ['id','filmeId']
 
 
-def load_model( file_name = ''):
+def load_model( file_name = 'decision_tree.pkl'):
     return pickle.load(open(file_name, "rb"))
+
+modelo = load_model('decision_tree.pkl')
 
 @app.route('/reco/<id>')
 def show_id(id):
@@ -24,12 +31,16 @@ def show_id(id):
 
 @app.route('/reco/', methods=['POST'])
 def get_recomendations():
+   
     dados = request.get_json()
-    # playload = np.array([dados[col] for col in colunas])
-    #playload = fit_transform([playload], feature_names=colunas)
-    #score = np.float64(modelo.predict(playload)[0])
-    if(dados):
-        return f'chegou aqui {dados}'
+    playload = np.array([dados[col] for col in colunas])
+    
+    playload = playload.reshape(1, -1)
+    score = np.float(modelo.predict(playload))
+    score = int(score)
+    if(score):
+        return f'O filme que recomendamos para você é {movies.iloc[score][1]}'
+
     else:
         return 'nada aqui'
     
